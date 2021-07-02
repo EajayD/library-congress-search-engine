d882455b41b611fe49168564b4a3c77b9fafc82f
SL <samanthl795@gmail.com>
2021-07-02 14:05:08 -0700

    Get value from text field

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var searchInputEl = $("#search-input-text");
+var formEl = $("#search-form");
+
+
 // Get the search results for a search query
 function getSearchResults(searchQuery, format) {
     var requestUrl = encodeURI("https://www.loc.gov/" + format + "/?q=" + searchQuery + "&fo=json");
@@ -13,3 +17,13 @@ function getSearchResults(searchQuery, format) {
 
 // A test
 getSearchResults("Civil War", "maps");
+
+function handleFormSubmit(event) {
+    // Prevent the default behavior
+    event.preventDefault();
+    console.log(searchInputEl.val())
+}
+
+// Submit event on the form
+//We can use .on('submit', ...) as the click event for the <form> element and pass handleFormSubmit() as the callback function:
+formEl.on('submit', handleFormSubmit);
